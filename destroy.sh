300fc78fcb3638f5500d754c2a2fa1014b5e3b60
Ronald den Otter <ronald.den.otter@gmail.com>
2020-04-05 10:42:54 +0200

    little changes

--- destroy.sh
+++ destroy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+docker stop omada-controller
 docker rm omada-controller
 docker rmi omada-controller:3.2.6-1
--- run.sh
+++ run.sh
@@ -3,15 +3,17 @@ mkdir -p /opt/tplink/OmadaController/data
 mkdir -p /opt/tplink/OmadaController/work
 mkdir -p /opt/tplink/OmadaController/logs
 
-chown -R 508:508 /opt/docker/tplink
+groupadd -g 508 omadad
+useradd -u 508 -g 508 -d /opt/tplink/OmadaController omadad
 
-omadadata="/opt/docker/tplink/OmadaController/data"
-omadawork="/opt/docker/tplink/OmadaController/work"
-omadalogs="/opt/docker/tplink/OmadaController/logs"
+chown -R 508:508 /opt/tplink/OmadaController
+
+omadadata="/opt/tplink/OmadaController/data"
+omadawork="/opt/tplink/OmadaController/work"
+omadalogs="/opt/tplink/OmadaController/logs"
 
 docker run -d \
 --name omada-controller \
 -e TZ=Europe/Amsterdam \
--e SMALL_FILES=true \
 -p 8088:8088   -p 8043:8043   -p 27001:27001/udp   -p 27002:27002   -p 29810:29810/udp   -p 29811:29811   -p 29812:29812   -p 29813:29813  \
--v $omadadata:/opt/tplink/OmadaController/data   -v $omadawork:/opt/tplink/OmadaController/work   -v $omadalogs:/opt/tplink/OmadaController/logs omada-controller:3.2.6 
+-v $omadadata:/opt/tplink/OmadaController/data   -v $omadawork:/opt/tplink/OmadaController/work   -v $omadalogs:/opt/tplink/OmadaController/logs omada-controller:3.2.6-1
